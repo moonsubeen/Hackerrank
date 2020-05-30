4513d1f3a7ad695b6d338757765e67d5402fec9a
moonsubeen <zx04090@naver.com>
2020-05-30 16:23:12 +0900

    stack 알고리즘 풀이 완료

--- src/com/day5/exception/Maximum_Element_Stack.java
+++ src/com/day5/exception/Maximum_Element_Stack.java
@@ -1,10 +1,53 @@
 package com.day5.exception;
 
+import java.io.*;
+import java.util.*;
+
 public class Maximum_Element_Stack {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		Stack<Myobject> stack = new Stack<Myobject>();
+		
+		int n = scan.nextInt();
+		int max = 0;
+		
+		for(int i = 0; i < n; i++)
+		{
+			int q = scan.nextInt();
+			if(q ==1)
+			{
+				int j = scan.nextInt();
+				if(max < j)
+					max = j;
+				stack.push(new Myobject(j, max));
+			}
+			else if(q==2)
+			{
+				stack.pop();
+				if(!stack.isEmpty())
+					{
+						max = stack.peek().max;
+					}
+				else
+					max = 0;
+			}
+			else if(q==3)
+			{
+				System.out.println(stack.peek().max);
+			}
+		}
 	}
-
+	
+	private static class Myobject
+	{
+		int value;
+		int max;
+		
+		Myobject(int value, int max)
+		{
+			this.value = value;
+			this.max = max;
+		}
+	}
 }
